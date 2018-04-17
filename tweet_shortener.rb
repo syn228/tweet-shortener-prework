53fb92ee8e82469e623f015c2ca42e792137aa17
Aaron Na <syn228@nyu.edu>
2018-04-17 19:13:55 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,7 +18,8 @@ def word_substituter(tweet)
   tweet.split(" ").collect do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
-    else word
+    else
+      word
     end
   end.join(" ")
 end
