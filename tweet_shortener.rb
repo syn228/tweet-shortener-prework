7c62cb3b8ebdcd2d569a72d01978d7b7cc4dff1e
Aaron Na <syn228@nyu.edu>
2018-04-17 17:58:57 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary
+  @substitutes = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    }
+end
+
+
+def word_substituter(tweet)
+  shortened = []
+  tweet.split(",").each do |word|
+    case word
+    when "#{@substitutes.keys}"
+      shortened << "#{@substitutes.values}"
+    else shortened << word
+    end
+  end
+  shortened = shortened.join(" ")
+  shortened
+    
+    
+    #{|words| words == "#{@substitutes.keys}" ? "#{@substitutes.values}" : words} 
+  #tweet.join(" ")
+    
+  
+end
