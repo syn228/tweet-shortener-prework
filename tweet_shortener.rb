3fdf7cec65a039987fe3450f42e943ac580becc7
Aaron Na <syn228@nyu.edu>
2018-04-17 18:52:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,7 +39,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   tweet.split(" ").collect do |set|
-    if set.length > 140
+    if selective_tweet_shortener(set).length > 140
        word_substituter(set)[0..140] + "..."
     else 
       set
